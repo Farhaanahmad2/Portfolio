9c3b62ccfe2a6d4a39d4f0b8541e9556912fe048
Farhaanahmad2 <farhanahmad32954@gmail.com>
2024-11-16 22:53:23 +0530

    add project

--- src/projects.jsx
+++ src/projects.jsx
@@ -35,6 +35,10 @@ const auth_des=`This user authentication system is built with MERN.It provide MA
                    with Mongoose based schema.`
 
 
+const portfolio_des=`A highly interactive portfolio built with modern web technologies:
+React for structure and seamless component management.
+GSAP and Framer Motion used for animations that bring every section to life.
+Locomotive Scroll for buttery-smooth scrolling and a sleek user experience.`
 
 
 
@@ -50,17 +54,19 @@ const senseai_git=`https://github.com/Farhaanahmad2/SenseAI`
 const game_git=`https://github.com/Farhaanahmad2/Simons_says_Game`
 
 
-const weather_git=`https://github.com/Farhaanahmad2/WeatherApp`
+// const pass_git=`https://github.com/Farhaanahmad2/Portfolio`
 
 const auth_git=`https://github.com/Farhaanahmad2/WeatherApp`
 
-
+const portfolio_git=`https://github.com/Farhaanahmad2/Portfolio`
 
 
 const hotel_link=`https://wanderlust-1-thok.onrender.com/listings`
 
 
 const senseai_link=`https://sense-ai-lilac.vercel.app/`
+
+const portfolio_link=`https://portfolio-nine-beta-14.vercel.app/`
 
 
 
@@ -94,11 +100,16 @@ const senseai_link=`https://sense-ai-lilac.vercel.app/`
 
     <main  className=' bg-black text-white w-[100vw] min-h-[180vh]  grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-8 py-3  sm:gap-10 sm:px-10 lg:gap-16 lg:px-16 lg:py-5 ' > 
        <Toggle2 className="m-2 "  inside="i1" tiltdiv="t1"  overlay="o1"  cursor="c1" color="#FFA500" projectimg={"A.PNG"}    para={hotel_des} title={"A HOTEL EXPLORER"} git={hotel_git}  link={hotel_link} />
-       <Toggle2 className="m-2 "  inside="i2" tiltdiv="t2"  overlay="o2"  cursor="c2" color="#BF00FF" projectimg={"S.png"}    para={senseai_des} title={"SENSE AI"} git={senseai_git}  link={senseai_link}  />
+       
+       <Toggle2 className="m-2 "  inside="i5" tiltdiv="t5"  overlay="o5"  cursor="c5" color="#20B2AA" projectimg={"portfolio.png"} para={portfolio_des} title={"MY PORTFOLIO"} git={portfolio_git} link={portfolio_link}    />
+
        <Toggle2 className="m-2 "  inside="i6" tiltdiv="t6"  overlay="o6"  cursor="c6" color="#FF1493" projectimg={"Auth.png"} para={auth_des} title={"USER AUTH & VERIFY"} git={auth_git}    />
+
+       <Toggle2 className="m-2 "  inside="i2" tiltdiv="t2"  overlay="o2"  cursor="c2" color="#BF00FF" projectimg={"S.png"}    para={senseai_des} title={"SENSE AI"} git={senseai_git}  link={senseai_link}  />
+
        <Toggle2 className="m-2 "  inside="i3" tiltdiv="t3"  overlay="o3"  cursor="c3" color="#00BFFF" projectimg={"P.png"}    para={pass_des}  title={"LOCK SHEILD"}    />
        <Toggle2 className="m-2 "  inside="i4" tiltdiv="t4"  overlay="o4"  cursor="c4" color="#98FF98" projectimg={"game2.png"} para={game_des} title={"SIMON SAYS GAME"} git={game_git}    />
-       <Toggle2 className="m-2 "  inside="i5" tiltdiv="t5"  overlay="o5"  cursor="c5" color="#20B2AA" projectimg={"weather1.png"} para={senseai_des} title={"WEATHER APP"} git={weather_git}    />
+       
        
        
        {/* <Toggle2/> */}
